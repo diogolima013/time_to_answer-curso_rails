df203a0f13a226a4a97ad881daa52951c6b283eb
Diogo <diogoslva0133@gmail.com>
2023-01-24 09:17:21 -0300

    add: adjusting Edit route for Admin

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index' #Dashboard
-    resources :admins, only: [:index] #Adminstradores
+    resources :admins, only: [:index, :edit] #Adminstradores
   end
   
   devise_for :admins

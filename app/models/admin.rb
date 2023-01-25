a7f05dd84d938f5fa4fa59c87cc691dfa8ed026b
Diogo <diogoslva0133@gmail.com>
2023-01-25 08:35:39 -0300

    add: pagination with kaminari

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,8 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  # Kaminary
+  paginates_per 5
 end

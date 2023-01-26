f68dff8b96984910ececf6f29c279b7dbe221db8
Diogo <diogoslva0133@gmail.com>
2023-01-26 11:25:04 -0300

    add: create task for anwsers
    the question.rb file is buggy

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :subject
   has_many :answers
-  accepts_nested_attributes_for :answers
+  accepts_nested_attributes_for :answers, allow_destroy: true
 end

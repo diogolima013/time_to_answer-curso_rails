08f06d21d1f8ffa9d4ed15d4528b67ca8bcd30b4
Diogo <diogoslva0133@gmail.com>
2023-02-01 08:55:19 -0300

    add: filtering questions by subject

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,6 +7,12 @@ class Question < ApplicationRecord
   paginates_per 5
 
   #Scope só serve pra fazer pesquisas no banco de dados
+  scope :_search_subject_, -> (page, subject_id){
+    includes(:answers)
+    .where(subject_id: subject_id)
+    .page(page)
+  } 
+
   scope :_search_, -> (page, term){
     includes(:answers)
     .where("lower(description) LIKE ?", "%#{term.downcase}%")
